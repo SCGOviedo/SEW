2f7973150cab36133e5509bbcca7c9036c87d210
Sergio <79648101+SCGOviedo@users.noreply.github.com>
2021-11-28 21:04:57 +0100

    Add files via upload

--- PRACTICA-3/Ejercicio-02/IdiomaNavegador.js
+++ PRACTICA-3/Ejercicio-02/IdiomaNavegador.js
@@ -1,3 +1,2 @@
 /* IdiomaNavegador.js */
-document.write("Idioma del navegador: ")
 document.write(infoNavegador.idioma);
--- PRACTICA-3/Ejercicio-02/NombreNavegador.js
+++ PRACTICA-3/Ejercicio-02/NombreNavegador.js
@@ -1,3 +1,2 @@
 /* NombreNavegador.js */
-document.write("Nombre del Navegador: ");
 document.write(infoNavegador.nombre);
