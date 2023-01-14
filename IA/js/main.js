0758ba48ed5dd33093d003abb5737d1eab14b36b
Sergio <79648101+SCGOviedo@users.noreply.github.com>
2023-01-14 13:57:41 +0100

    Update main.js

--- IA/js/main.js
+++ IA/js/main.js
@@ -18,9 +18,9 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var boxA = Bodies.circle(400, 200, 80, 80);
+var boxB = Bodies.circle(450, 50, 80, 80);
+var ground = Bodies.rectangle(0, gameSize.w, gameSize.h-50, gameSize.h, { isStatic: true });
 
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
